5447782051e8922043bac7c24f2769b5bad60f3e
Nan <xnone0104@gmail.com>
2019-05-09 23:47:45 +0800

    finish problem 20

--- problem020.py
+++ problem020.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 Author: Nan <xnone0104@gmail.com>
 
@@ -13,3 +15,22 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!
 
 """
+
+import math
+
+def factorial(n):
+	fac = 1
+	for i in range(n):
+		fac *= i+1
+	return fac
+
+def factorial_digit_sum(fac):
+	result = 0
+	for i in str(fac):
+		result += int(i)
+	return result
+
+fac = factorial(100)
+result = factorial_digit_sum(fac)
+
+print(result)
